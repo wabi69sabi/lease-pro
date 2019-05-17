6354707cc6d2b5560e833fc421528a5840e23d13
Lenovo <wabisabisf@gmail.com>
2019-05-16 17:07:42 -0700

    added factory bot and user validators plus spec folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: [:owner, :manager, :tenant]
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
