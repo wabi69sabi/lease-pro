dd08f080da19e5685611393340bc1e9cf2e08959
Lenovo <wabisabisf@gmail.com>
2019-05-16 18:55:45 -0700

    added unit testing for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = FactoryBot.build(:user)
+  end
+
+  it "should not be valid without a first_name" do
+    @user.first_name = nil
+    @user.should_not be_valid
+  end
+
+  it "should allow to assign a role" do
+    @user.role = "tenant"
+    expect(@user.tenant?).to be true
+  end
+
+  it "should reject an invalid role" do
+    expect { @user.role = "foo" }.to raise_error ("'foo' is not a valid role")
+  end
 end
